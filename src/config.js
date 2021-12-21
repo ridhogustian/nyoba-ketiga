dbfa565c076df84cf0e8c0aeff1e9cf6f5075e90
ridhogustian <ridhogustian1@gmail.com>
2021-12-21 21:06:55 +0700

    initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// using module.exports here so gatsby-config can read it
+module.exports = {
+  // TODO: Replace next line with your own public API key
+  builderAPIKey: 'aeabf91c3b4c44229d1ed9f4a6caa224'
+};
